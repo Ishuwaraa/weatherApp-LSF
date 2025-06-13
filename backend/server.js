e104d3e8597582f90d5e20e4684ad35691d6ff1c
Ishuwara Avarjana <ishuwarawedage@gmail.com>
2025-06-13 21:38:38 +0530

    Vercel config file update

--- backend/server.js
+++ backend/server.js
@@ -16,13 +16,13 @@ app.use(cors({
     credentials: true,
 }));
 
+app.get('/', (req, res) => {
+    res.status(200).send("Welcome to the Weather API")
+})
+app.use('/api/weather', weatherRoutes);
+
 app.listen(process.env.PORT, () => {
     console.log(`Server running on port: ${process.env.PORT}`);
 }).on('error', (e) => {
     console.log('Error starting server:', e.message);
 });
-
-app.get('/', (req, res) => {
-    res.status(200).json("Welcome to the Weather API")
-})
-app.use('/api/weather', weatherRoutes);
